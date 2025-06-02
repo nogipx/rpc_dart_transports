d9833c47f9fbd832fd529c9bf6558897d522e538
nogipx <nogipx@gmail.com>
2025-06-03 01:50:02 +0300

    wip

--- rpc_dart/lib/src/logs/_logs.dart
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: 2025 Karim "nogipx" Mamatkazin <nogipx@gmail.com>
-//
-// SPDX-License-Identifier: LGPL-3.0-or-later
-
-part 'rpc_logger.dart';
-part 'rpc_logger_colors.dart';
-part 'default_rpc_logger.dart';
-part 'rpc_logger_registry.dart';
-part 'rpc_logger_settings.dart';
--- rpc_dart/lib/src/utils/_index.dart
+++
@@ -1,5 +0,0 @@
-// SPDX-FileCopyrightText: 2025 Karim "nogipx" Mamatkazin <nogipx@gmail.com>
-//
-// SPDX-License-Identifier: LGPL-3.0-or-later
-
-export 'stream_distributor.dart';
